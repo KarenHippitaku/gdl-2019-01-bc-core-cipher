e7bf2af5cc0ad91d82d40df21d235b79ee00f845
KarenHippitaku <hippitaku@gmail.com>
2019-02-06 13:36:59 -0600

    Botones activos     -Se agregaron las funciones para los botones restantes     -Los botones cambian de seccion correctamente     -Los botones de cifrado continuan funcionando con normalidad

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+function entrar(){
+    document.getElementById("bloqueInicio").style.display = "none";
+    document.getElementById("bloqueCifrado").style.display = "block";
+    document.getElementById("bloqueInformacion").style.display = "none";
+    document.getElementById("bloqueEnviado").style.display = "none";
+  }
+  document.getElementById("botonEntrar").addEventListener("click", entrar);
+
+function irAyuda(){
+  document.getElementById("bloqueInicio").style.display = "none";
+  document.getElementById("bloqueCifrado").style.display = "none";
+  document.getElementById("bloqueInformacion").style.display = "block";
+  document.getElementById("bloqueEnviado").style.display = "none";
+  }
+  document.getElementById("botonAyuda").addEventListener("click", irAyuda);
+
+function salir(){
+    document.getElementById("bloqueInicio").style.display = "none";
+    document.getElementById("bloqueCifrado").style.display = "block";
+    document.getElementById("bloqueInformacion").style.display = "none";
+    document.getElementById("bloqueEnviado").style.display = "none";
+  }
+  document.getElementById("botonSalir").addEventListener("click", salir);
+
 function encode(){
     let string = document.getElementById("string").value;
     let offset = Number(document.getElementById("offset").value);
@@ -6,10 +30,26 @@ function encode(){
   }
   document.getElementById("botonCifrar").addEventListener("click",encode);
 
-  function decode(){
-    let string = document.getElementById("string").value;
-    let offset = Number(document.getElementById("offset").value);
-    let output = window.cipher.decode(offset, string);
-    document.getElementById("newString").innerHTML = output;
+function decode(){
+  let string = document.getElementById("string").value;
+  let offset = Number(document.getElementById("offset").value);
+  let output = window.cipher.decode(offset, string);
+  document.getElementById("newString").innerHTML = output;
+}
+document.getElementById("botonDescifrar").addEventListener("click",decode);
+
+function enviar(){
+  document.getElementById("bloqueInicio").style.display = "none";
+  document.getElementById("bloqueCifrado").style.display = "none";
+  document.getElementById("bloqueInformacion").style.display = "none";
+  document.getElementById("bloqueEnviado").style.display = "block";
   }
-  document.getElementById("botonDescifrar").addEventListener("click",decode);
+  document.getElementById("botonEnviar").addEventListener("click", enviar);
+
+function volverACifrar(){
+  document.getElementById("bloqueInicio").style.display = "none";
+  document.getElementById("bloqueCifrado").style.display = "block";
+  document.getElementById("bloqueInformacion").style.display = "none";
+  document.getElementById("bloqueEnviado").style.display = "none";
+  }
+  document.getElementById("botonVolver").addEventListener("click", volverACifrar);
